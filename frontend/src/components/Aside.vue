9d7ecbd35113f3882c7a286ba83cb7ca590f6b22
kenny <baoboo233@gmail.com>
2021-01-20 02:17:37 -0500

    update aside

--- frontend/src/components/Aside.vue
+++ frontend/src/components/Aside.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="aside">
-    <h1>This is the Sidebar</h1>
-    <h1>This is the Sidebar</h1>
-    <h1>This is the Sidebar</h1>
-    <h1>This is the Sidebar</h1>
-    <h1>This is the Sidebar</h1>
-    <h1>This is the Sidebar</h1>
-    <h1>This is the Sidebar</h1>
+    <el-menu
+      background-color="#82C275"
+      text-color="#fff"
+      active-text-color="#5D8B57">
+      <el-menu-item index="1">
+        <i class="el-icon-location"></i>
+        <span slot="title">任务大厅</span>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <i class="el-icon-document"></i>
+        <span slot="title">信息管理</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-setting"></i>
+        <span slot="title">账号设置</span>
+      </el-menu-item>
+      <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>备用</span>
+        </template>
+        <el-menu-item index="4-1">
+          <span>选项一</span>
+        </el-menu-item>
+        <el-menu-item index="4-2">
+          <span>选项二</span>
+        </el-menu-item>
+      </el-submenu>
+    </el-menu>
   </div>
 </template>
 
