c0e31a2e115232c1bfa1be859c18e0a90242e0e5
kenny <baoboo233@gmail.com>
2021-01-31 21:50:25 -0500

    dropdown update

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -4,14 +4,23 @@
       <el-col :span="3" :offset="1" id="logo">
         <el-image
           :src="images.logo"
-          :fit="fit">
+          :fit="fit" class="logo-image">
         </el-image>
       </el-col>
-      <el-col :span="1" :offset="18" id="avatar">
-        <el-image
-          :src="images.avatar"
-          :fit="fit">
-        </el-image>
+      <el-col :span="2" :offset="18" id="avatar">
+        <el-dropdown trigger="click">
+          <span class="el-dropdown-link">
+            <el-image
+              :src="images.avatar"
+              :fit="fit" class="avatar-image">
+            </el-image>
+            <i class="el-icon-arrow-down arrow-down"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>用户信息</el-dropdown-item>
+            <el-dropdown-item>退出当前账号</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
       </el-col>
     </el-row>
   </div>
@@ -36,12 +45,12 @@ export default {
   height: 70px;
   background: #6ba760;
 }
-#logo > .el-image {
+.logo-image {
   margin-top: 10px;
   width: 181px;
   height: 50px;
 }
-#avatar > .el-image {
+.avatar-image {
   margin-top: 8px;
   width: 50px;
   height: 50px;
@@ -49,5 +58,14 @@ export default {
   border: 2px solid #eee;
   background: #eee;
 }
-
+.arrow-down {
+  position: absolute;
+  top: 28px;
+  margin-left: 6px;
+  color: #eee;
+}
+.el-dropdown-menu__item:hover {
+  background: #F5F7FA;
+  color: #606266;
+}
 </style>
